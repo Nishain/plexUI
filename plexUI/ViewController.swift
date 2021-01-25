85648105f410834cd75e511200717337f92cdd99
Nishain <nishain.atomic@gmail.com>
2021-01-25 20:52:46 +0530

    fix missing button view container bug

--- plexUI/ViewController.swift
+++ plexUI/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var policyTerms: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let attributedString = NSMutableAttributedString(string: policyTerms.text!)
         let range1 =  (policyTerms.text! as NSString).range(of: "Terms of Service")
-        attributedString.addAttribute(NSAttributedString.Key.foregroundColor,value:UIColor.red, range: range1)
+        let range2 =  (policyTerms.text! as NSString).range(of: "Privacy Policy")
+        let color = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor,value:color, range: range1)
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor,value:color, range: range2)
         policyTerms.attributedText = attributedString
+        for uiView in stackView.subviews {
+            uiView.layer.cornerRadius = 10
+            
+        }
     }
     
     
