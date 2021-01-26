7103778fe41f174ae6800a8cc522edf84cb3a82f
Nishain <nishain.atomic@gmail.com>
2021-01-26 10:23:59 +0530

    added image assets,colored privacy notice, layout fixing

--- plexUI/ViewController.swift
+++ plexUI/ViewController.swift
@@ -12,15 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let attributedString = NSMutableAttributedString(string: policyTerms.text!)
-        let range1 =  (policyTerms.text! as NSString).range(of: "Terms of Service")
-        let range2 =  (policyTerms.text! as NSString).range(of: "Privacy Policy")
-        let color = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
-        attributedString.addAttribute(NSAttributedString.Key.foregroundColor,value:color, range: range1)
-        attributedString.addAttribute(NSAttributedString.Key.foregroundColor,value:color, range: range2)
-        policyTerms.attributedText = attributedString
         for uiView in stackView.subviews {
-            uiView.layer.cornerRadius = 10
+            uiView.layer.cornerRadius = 6
             
         }
     }
